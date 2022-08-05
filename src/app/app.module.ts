56c450bd8ed9b763271963dbed988774885ba716
Justin MacDonald <justinmacdonald30@gmail.com>
2022-08-04 21:13:35 -0500

    Created Base Structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { MovieComponent } from './public/movie/movie.component';
+import { MovieDetailsComponent } from './public/movie/movie-details.component';
+import { CastDetailsComponent } from './public/cast/cast-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieComponent,
+    MovieDetailsComponent,
+    CastDetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CoreModule,
+    SharedModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
